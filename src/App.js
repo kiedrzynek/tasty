173e098700ae9c11c169cc3f7853c11e3f3f218d
Kamila Kiedrzynek <kamilakiedrzynek@gmail.com>
2020-12-21 18:31:45 +0100

    Pass the props into Recipe component

--- src/App.js
+++ src/App.js
@@ -26,7 +26,11 @@ const App = () => {
         <button type="submit" className="search-button">Search</button>
       </form>
       {recipes.map(recipe => (
-        <Recipe/>
+        <Recipe 
+          title={recipe.recipe.label} 
+          calories={recipe.recipe.calories} 
+          image={recipe.recipe.image}
+        />
       ))}
     </div>
   );
